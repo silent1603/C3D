ecb42bcfaa8bbb48cb23d88fcaa0ca76e397d463
silent1603 <tamgiangbui@gmail.com>
2022-04-29 14:06:42 +0700

    add base class

---
+++ Maze/main.h
@@ -0,0 +1,15 @@
+#ifdef MAIN_H
+#define MAIN_H
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <array>
+#include <vector>
+#include <iterator>
+#include <functional>
+#include <memory>
+#include <cassert>
+#ifdef _WIN32
+    #include <Windows.h>
+#endif
+#endif
